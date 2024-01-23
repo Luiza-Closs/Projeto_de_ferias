c08dc4cad2c2460f7a14545d1a275f0edaa813bd
Luiza Closs <luizacloss.2205@gmail.com>
2024-01-22 22:50:38 -0300

    22012024

---
+++ conversor_binario/script.js
@@ -0,0 +1,27 @@
+const descimalInput = document.getElementById("descimal");
+const binarioInput = document.getElementById("binario");
+
+const inputs = document.getElementsByClassName("inputs");
+
+for (let i = 0; i < inputs.length; i++) {
+    let input = inputs[i];
+
+    input.addEventListener("input", function (e) {
+        let value = parseFloat(e.target.value);
+        console.log("ativou a função")
+        
+        switch (e.target.name) {
+            case "descimal":
+                console.log(value)
+                let binarionum = value.toString(2)
+                binarioInput.value = binarionum;
+                break;
+            case "binario":
+                console.log(value)
+                let binario = value.toString();
+                let descimalnum = parseInt(binario, 2)
+                descimalInput.value = descimalnum
+                break;
+        }
+    });
+}
