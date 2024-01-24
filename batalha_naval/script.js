02d791092d8c3e790943061aa82fa34fad2bf92e
Luiza Closs <luizacloss.2205@gmail.com>
2024-01-24 15:46:59 -0300

    2401202402

--- batalha_naval/script.js
+++ batalha_naval/script.js
@@ -3,6 +3,7 @@ const telaJogo = document.getElementById('Jogo')
 // Definir tamanho do tabuleiro
 const divbtns = document.createElement('div')
 divbtns.className = "row col-12"
+let tamanho = 0
 
 const tipos = [ "facil", "médio", "dificil" ]
 tipos.forEach(tipo =>{
@@ -13,6 +14,24 @@ tipos.forEach(tipo =>{
     btn.className = "form-control btn"
     btn.name = tipo
     btn.value = tipo
+
+    btn.addEventListener('click', function(){
+        switch(tipo){
+            case "facil":
+                tamanho = 8
+                break
+            case "médio":
+                tamanho = 10
+                break
+            case "dificil":
+                tamanho = 13
+                break
+        }
+        console.log(tamanho)
+        divbtns.remove()
+        criarTabela(tamanho)
+
+    })
     div.appendChild(btn)
     divbtns.appendChild(div)
 })
@@ -20,6 +39,22 @@ tipos.forEach(tipo =>{
 telaJogo.appendChild(divbtns)
 
 // Criar tabela
+function criarTabela(tamanho){
+    const divTable = document.createElement('div')
+    for (let i = 0; i < tamanho; i++) {
+        const row = document.createElement('div')
+        row.className = "row"
+        for (let j = 0; j < tamanho; j++) {
+            const cell = document.createElement('div')
+            cell.className = 'cell col'
+            cell.dataset.row = i
+            cell.dataset.col = j
+            row.appendChild(cell)
+        }
+        divTable.appendChild(row)
+    }
+    telaJogo.appendChild(divTable)
+}
 
 // Colocar Barcos
 
